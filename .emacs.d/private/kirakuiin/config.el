7d1eb594b48a066403379edbc7418589df974fbb
wang.zhuowei <wang.zhuowe@foxmail.com>
2021-04-04 01:34:00 +0800

    [emacs]更新配置文件

--- .emacs.d/private/kirakuiin/config.el
+++ .emacs.d/private/kirakuiin/config.el
@@ -22,6 +22,8 @@
           (lambda ()
             (message "org-mode-hook...")
             (setq org-link-search-must-match-exact-headline nil ;; org文件链接跳转不再仅匹配标题
+                  org-enforce-todo-dependencies t ;; 子任务未完成父任务无法完成
+                  org-enforce-todo-checkbox-dependencies t ;; 子选框未完成任务无法完成
                   )))
 
 ;; C++
